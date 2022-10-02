db8279483ef1748d90dd1ca8befe691bbb79b561
Alex Silva <alexcssilva@gmail.com>
2022-10-02 19:00:34 -0300

    feat: update scraper.py - scrape_noticia - Desafio 04 concluido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,37 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css(".entry-title ::attr(href)").get()
+    title = selector.css(".entry-title ::attr(rel)").get()
+    timestamp = selector.css(".meta-date ::text").get()
+    writer = selector.css(".url a::text)").get()
+    comments_count = selector.css("#comments h5::text").get()
+    if comments_count is None:
+        comments_count = 0
+
+    summary = "".join(
+        selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+    ).strip()
+
+    tags = selector.css("a[rel=tag]::text").getAll()
+    if tags is None:
+        tags = []
+
+    category = selector.css(".label ::text").get()
+
+    scrape = [
+        url,
+        title,
+        timestamp,
+        writer,
+        comments_count,
+        summary,
+        tags,
+        category,
+    ]
+
+    return scrape
 
 
 # Requisito 5
